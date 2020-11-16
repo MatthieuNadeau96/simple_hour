ed86ff901f5ae5918355d67886f39bdf1c3ea663
Matthieu <matthieunadeau96@gmail.com>
2020-11-16 12:46:46 -0500

    Added some styling

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
     print(currentHourOfDay);
     _autoScrollController.scrollToIndex(
       10,
-      preferPosition: AutoScrollPosition.middle,
+      preferPosition: AutoScrollPosition.begin,
       duration: Duration(milliseconds: 2000),
     );
   }
@@ -32,66 +32,100 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: screenSize.height,
-          width: screenSize.width,
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  controller: _autoScrollController,
-                  children: List.generate(
-                    hours.length,
-                    (i) => AutoScrollTag(
-                      key: ValueKey(i),
-                      controller: _autoScrollController,
-                      index: i,
-                      child: Container(
-                        height: screenSize.height / 5,
-                        margin: EdgeInsets.symmetric(vertical: 5),
-                        color: Colors.grey[300],
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            RichText(
-                              text: TextSpan(
-                                  text: '${hours[i].hour}',
-                                  style: TextStyle(
-                                    fontSize: 25,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w300,
-                                  ),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                      text: '${hours[i].meridiem}',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                  ]),
-                            ),
-                            Flexible(
-                              child: Container(
-                                height: 60,
-                                color: Colors.white,
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 30,
+              // vertical: 30,
+            ),
+            height: screenSize.height,
+            width: screenSize.width,
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    controller: _autoScrollController,
+                    children: List.generate(
+                      hours.length,
+                      (i) => AutoScrollTag(
+                        key: ValueKey(i),
+                        controller: _autoScrollController,
+                        index: i,
+                        child: Container(
+                          height: screenSize.height / 6,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            border: i + 1 == currentHourOfDay
+                                ? Border.all(
+                                    color: Colors.grey,
+                                    width: 2,
+                                  )
+                                : null,
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(right: 15),
+                                  child: RichText(
+                                    text: TextSpan(
+                                        text: '${hours[i].hour}',
+                                        style: TextStyle(
+                                          fontSize: 25,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                            text: '${hours[i].meridiem}',
+                                            style: TextStyle(
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                        ]),
                                   ),
                                 ),
-                              ),
+                                Expanded(
+                                  child: Center(
+                                    child: Container(
+                                      width: 200,
+                                      height: 60,
+                                      color: Colors.grey[100],
+                                      child: TextField(
+                                        decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  // color: Colors.blue[100],
+                                  height: 50,
+                                  width: 50,
+                                  child: Icon(
+                                    Icons.check,
+                                    size: 40,
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
