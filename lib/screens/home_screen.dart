23b1edd5cc033e6b6cbca2d70ca7c914ced1063c
Matthieu <matthieunadeau96@gmail.com>
2020-11-16 12:06:08 -0500

    Added an automatic scroll to the current hour of the day every time the app has launched

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 import 'package:simple_hour/data/hour_data.dart';
 import 'package:simple_hour/models/hour_model.dart';
 
@@ -9,64 +11,81 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<HourModel> hours = List<HourModel>();
+  AutoScrollController _autoScrollController = AutoScrollController();
+  final dataKey = new GlobalKey();
+
+  int currentHourOfDay = TimeOfDay.now().hour;
 
   @override
   void initState() {
     super.initState();
     hours = getHours();
+    print(currentHourOfDay);
+    _autoScrollController.scrollToIndex(
+      10,
+      preferPosition: AutoScrollPosition.middle,
+      duration: Duration(milliseconds: 2000),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          height: MediaQuery.of(context).size.height,
+          height: screenSize.height,
+          width: screenSize.width,
           child: Column(
             children: [
               Expanded(
-                child: GridView.count(
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 2,
-                  crossAxisCount: 1,
+                child: ListView(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  controller: _autoScrollController,
                   children: List.generate(
                     hours.length,
-                    (index) => Container(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      color: Colors.grey[300],
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                                text: '${hours[index].hour}',
-                                style: TextStyle(
-                                  fontSize: 25,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: '${hours[index].meridiem}',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                    ),
+                    (i) => AutoScrollTag(
+                      key: ValueKey(i),
+                      controller: _autoScrollController,
+                      index: i,
+                      child: Container(
+                        height: screenSize.height / 5,
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        color: Colors.grey[300],
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                  text: '${hours[i].hour}',
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w300,
                                   ),
-                                ]),
-                          ),
-                          Flexible(
-                            child: Container(
-                              height: 60,
-                              color: Colors.white,
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: '${hours[i].meridiem}',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ]),
+                            ),
+                            Flexible(
+                              child: Container(
+                                height: 60,
+                                color: Colors.white,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
